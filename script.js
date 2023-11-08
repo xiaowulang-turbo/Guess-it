00dc092f157bd692d5df9a9a5fefe483aa1cb3a1
xiaowulang-turbo <93776841+xiaowulang-turbo@users.noreply.github.com>
2023-11-08 23:00:56 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,67 @@
+"use strict";
+
+/*
+console.log(document.querySelector('.message').textContent);
+//document.querySelector('.message').textContent = '🎉 Good Guess!';
+document.querySelector('.number').textContent = 23;
+
+document.querySelector('.score').textContent = 26;
+
+document.querySelector('.guess').value = 45;
+*/
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector(".number").textContent = secretNumber;
+let score = 20;
+let highScore = 0;
+
+//document.querySelector('.highscore').textContent = highScore;
+
+const displayMessage = function (message) {
+  document.querySelector(".message").textContent = message;
+};
+
+document.querySelector(".check").addEventListener("click", function () {
+  let guess = Number(document.querySelector(".guess").value);
+  console.log(secretNumber, typeof secretNumber);
+  console.log(guess, typeof guess);
+  if (!guess) {
+    displayMessage("⛔ No number!");
+  } else {
+    if (guess !== secretNumber) {
+      displayMessage(guess > secretNumber ? "📈 Too high!" : "Too low!");
+      if (score > 1) {
+        score--;
+        document.querySelector(".score").textContent = score;
+      } else {
+        score = 0;
+        document.querySelector(".score").textContent = score;
+        displayMessage("💥 You lose the game!");
+      }
+    } else {
+      displayMessage("🎉 Correct number!");
+      document.querySelector("body").style.backgroundColor = "#60b347";
+      document.querySelector(".number").style.width = "30rem";
+      if (score > highScore) {
+        highScore = score;
+        document.querySelector(".highscore").textContent = highScore;
+      }
+    }
+  }
+});
+
+document.querySelector(".again").addEventListener("click", function () {
+  let guess = Number((document.querySelector(".guess").value = ""));
+  displayMessage("Start guessing...");
+  document.querySelector("body").style.backgroundColor = "#000";
+  document.querySelector(".number").style.width = "15rem";
+  score = 20;
+  document.querySelector(".score").textContent = score;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector(".number").textContent = secretNumber;
+});
+
+console.log(1 > "0");
+console.log(1 > "1");
+console.log(1 === "1");
+console.log(1 < "1");
